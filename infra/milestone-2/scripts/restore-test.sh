bdf5e90a4348ce4e60abb43b7f6571fbf80db67d
Ben Pelo <benjaminpelo.business@gmail.com>
2026-08-21 19:49:51 -0600

    Harden milestone 2 infrastructure checks

--- infra/milestone-2/scripts/restore-test.sh
+++ infra/milestone-2/scripts/restore-test.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -eu
 
-SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
-# shellcheck source=common.sh
+SCRIPT_DIR=$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd)
+# shellcheck disable=SC1091
 . "$SCRIPT_DIR/common.sh"
 
 [ "$#" -eq 2 ] || die "usage: restore-test.sh BACKUP_DIRECTORY EMPTY_RESTORE_ROOT"
